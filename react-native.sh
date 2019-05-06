b0564d62003bbd0aa20136b0a7d3c36f31eef21d
lindboe <lindboe@users.noreply.github.com>
2019-05-06 13:46:16 -0700

    Scripts for different ways of using react native on linux

---
+++ react-native.sh
@@ -0,0 +1,5 @@
+sudo apt install -y nodejs
+npm i -g react-native-cli
+echo "Remember to install the JDK! I'm not doing that for you."
+echo "You might also want Android Studio and/or Expo."
+sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
