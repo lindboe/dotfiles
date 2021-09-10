2f237a075f356de36a19ae4efe3420db1aef3c49
Lizzi Lindboe <lindboe@users.noreply.github.com>
2021-09-10 10:18:41 -0700

    Add reminder about android licenses

--- react_native_osx.sh
+++ react_native_osx.sh
@@ -6,3 +6,5 @@ brew install --cask adoptopenjdk/openjdk/adoptopenjdk8
 
 npm install -g yarn
 npm install -g react-native-cli
+
+echo "Install Android Studio, open a project, and run `sdkmanager --licenses`"
