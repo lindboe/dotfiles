d1eaad775cd02bd7bc48d999a8eaacf4c9bdde18
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-03-12 12:14:37 -0700

    Update fix completions permissions script and remove problematic line from nvm setup that was causing error complete:13: command not found: compdef

--- setup/fix-completions-permissions.sh
+++ setup/fix-completions-permissions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eof pipefail
 
-# Not sure if this is needed now?
-# chmod -R go-w '/opt/homebrew/share'
+chmod -R go-w '/opt/homebrew/share'
+# This isn't listed by compaudit in my laaatest setup
 # chmod -R go-w '/opt/homebrew/share/zsh'
 
