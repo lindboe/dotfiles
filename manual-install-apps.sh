564fae6606a522ca9273caa1cea136d7d90ff412
Lizzi Lindboe <lindboe@users.noreply.github.com>
2024-03-16 14:37:58 -0700

    Stuff

---
+++ manual-install-apps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eof pipefail
+
+# For apps that I don't (yet) use homebrew for, most likely because I haven't
+# tested it out yet, or don't want to deal with path or potential upgrade
+# issues.
+# This is just a convenient list to help me quickly remember all the apps I'd want.
+
+echo 'Visual Studio Code: https://code.visualstudio.com/download'
+echo 'Gitify: https://gitify.io'
+# react-native dev
+echo 'XCode: https://apps.apple.com/app/xcode'
+echo 'Android Studio: https://developer.android.com/studio'
+echo 'Android File Transfer: https://android.com/filetransfer'
+echo 'Vysor: https://vysor.io/download'
+echo 'RocketSim: https://rocketsim.app'
+echo 'Discord: https://discord.com/download'
+# productivity
+echo 'Kiwi: https://kiwiforgmail.com/download'
+echo 'Notion: https://notion.so'
+# utility
+echo 'Stay: https://apps.apple.com/app/stay'
+echo 'Lunar: https://lunar.fyi'
+echo 'Homerow: https://homerow.app'
+echo 'Superkey: https://superkey.app'
+# Infinite Red
+echo 'Clue: https://github.com/infinitered/next-clue-next/releases'
+echo 'Multi: https://multi.app/'
+echo 'Insta360 link controller: https://www.insta360.com/download/insta360-link'
